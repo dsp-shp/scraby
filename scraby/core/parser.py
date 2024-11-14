f5c5ac42bfdbf0f262b028f8ca6000e00bcdb087
Ivan <dsp_shp@icloud.com>
2024-11-14 17:22:48 +0300

    fix(parser): add HTML tags parsing and printing

--- scraby/core/parser.py
+++ scraby/core/parser.py
@@ -1,11 +1,153 @@
 from copy import deepcopy
+from enum import Enum
 import typing as t
+import re
 
 from sqlglot import exp, Generator
 from sqlglot.dialects import Dialect
 from sqlglot.parser import Parser, ParseError
 from sqlglot.tokens import Token, TokenType, Tokenizer, TokenError
 
+
+class HTMLTag(Enum):
+    """ Enumeration of HTML tags """
+    ABBREVIATION = 'abbr'
+    ACRONYM = 'acronym'
+    ADDRESS = 'address'
+    ANCHOR = 'a'
+    APPLET = 'applet'
+    AREA = 'area'
+    ARTICLE = 'article'
+    ASIDE = 'aside'
+    AUDIO = 'audio'
+    BASE = 'base'
+    BASEFONT = 'basefont'
+    BDI = 'bdi'
+    BDO = 'bdo'
+    BGSOUND = 'bgsound'
+    BIG = 'big'
+    BLOCKQUOTE = 'blockquote'
+    BODY = 'body'
+    BOLD = 'b'
+    BREAK = 'br'
+    BUTTON = 'button'
+    CAPTION = 'caption'
+    CANVAS = 'canvas'
+    CENTER = 'center'
+    CITE = 'cite'
+    CODE = 'code'
+    COLGROUP = 'colgroup'
+    COLUMN = 'col'
+    DATA = 'data'
+    DATALIST = 'datalist'
+    DD = 'dd'
+    DEFINE = 'dfn'
+    DELETE = 'del'
+    DETAILS = 'details'
+    DIALOG = 'dialog'
+    DIR = 'dir'
+    DIV = 'div'
+    DL = 'dl'
+    DT = 'dt'
+    EMBED = 'embed'
+    FIELDSET = 'fieldset'
+    FIGCAPTION = 'figcaption'
+    FIGURE = 'figure'
+    FONT = 'font'
+    FOOTER = 'footer'
+    FORM = 'form'
+    FRAME = 'frame'
+    FRAMESET = 'frameset'
+    HEAD = 'head'
+    HEADER = 'header'
+    HEADING1 = 'h1'
+    HEADING2 = 'h2'
+    HEADING3 = 'h3'
+    HEADING4 = 'h4'
+    HEADING5 = 'h5'
+    HEADING6 = 'h6'
+    HGROUP = 'hgroup'
+    HR = 'hr'
+    HTML = 'html'
+    IFRAMES = 'iframe'
+    IMAGE = 'img'
+    INPUT = 'input'
+    INS = 'ins'
+    ISINDEX = 'isindex'
+    ITALIC = 'i'
+    KBD = 'kbd'
+    KEYGEN = 'keygen'
+    LABEL = 'label'
+    LEGEND = 'legend'
+    LIST = 'li'
+    MAIN = 'main'
+    MARK = 'mark'
+    MARQUEE = 'marquee'
+    MENUITEM = 'menuitem'
+    META = 'meta'
+    METER = 'meter'
+    NAV = 'nav'
+    NOBREAK = 'nobr'
+    NOEMBED = 'noembed'
+    NOSCRIPT = 'noscript'
+    OBJECT = 'object'
+    OPTGROUP = 'optgroup'
+    OPTION = 'option'
+    OUTPUT = 'output'
+    PARAGRAPHS = 'p'
+    PARAM = 'param'
+    PHRASE = 'em'
+    PRE = 'pre'
+    PROGRESS = 'progress'
+    Q = 'q'
+    RP = 'rp'
+    RT = 'rt'
+    RUBY = 'ruby'
+    S = 's'
+    SAMP = 'samp'
+    SCRIPT = 'script'
+    SECTION = 'section'
+    SMALL = 'small'
+    SOURCE = 'source'
+    SPACER = 'spacer'
+    SPAN = 'span'
+    STRIKE = 'strike'
+    STRONG = 'strong'
+    STYLE = 'tagname'
+    SUMMARY = 'summary'
+    SVG = 'svg'
+    TABLE = 'table'
+    TBODY = 'tbody'
+    TD = 'td'
+    TEMPLATE = 'template'
+    TFOOT = 'tfoot'
+    TH = 'th'
+    THEAD = 'thead'
+    TIME = 'time'
+    TITLE = 'title'
+    TR = 'tr'
+    TRACK = 'track'
+    TT = 'tt'
+    UNDERLINE = 'u'
+    VAR = 'var'
+    VIDEO = 'video'
+    WBR = 'wbr'
+    XMP = 'xmp'
+
+    # def __str__(self) -> str:
+    #     return self.value if isinstance(self, HTMLTag) else self
+
+    @classmethod
+    def values(cls) -> list[str]:
+        return [x.value for x in cls]
+
+class TagAttr(exp.Expression):
+    """ Expression of the HTML tag' attribute """
+    arg_types = {"this": True, "expression": False, "is_regular": False}
+
+class Tag(exp.Expression):
+    """ Expression of the HTML tag """
+    arg_types = {"this": True, "expressions": False}
 
 class LoadPage(exp.Expression):
     """ Expression of the page loading function LOAD """
@@ -41,12 +183,11 @@ class ScrabyDialect(Dialect):
         TokenType.IDENTIFIER,
         TokenType.VAR,
         TokenType.DIV,
-        #  TODO: add all posible tokens
+        #  TODO: add all other posible tokens
     }
     """ All allowed keywords including single-symbol tokens """
 
-    #  TODO: add all other
-    TAGS = ("a", "div", "span")
+    TAGS = {*HTMLTag.__members__}
     """ All posible HTML tags """
 
     class ScrabyGenerator(Generator):
@@ -57,9 +198,44 @@ class ScrabyDialect(Dialect):
             expr = f", {expr}" if expr and int(expr) > 0 else ""
             return f"LOAD({this}{expr})"
 
+        def tag_sql(self, expression: Tag) -> str:
+            this = expression.this.value
+            attributes = ' '.join(map(
+                lambda x: f"{x.this}=\"{x.expression}\"",
+                expression.args.get('expressions', [])
+            ))
+            attributes = f" {attributes}" if attributes else ""
+            return f"<{this}{attributes}>"
+
     class ScrabyParser(Parser):
         """ Custom parser used to support correct parsing of new features """
         FUNC_TOKENS = {*Parser.FUNC_TOKENS, TokenType.LOAD,}
+        #  INFO: there are usages of a default exp.Tag in Parser
+        DB_CREATABLES = {x for x in Parser.DB_CREATABLES if x not in (TokenType.TAG,)}
+        CREATABLES = {x for x in Parser.CREATABLES if x not in (TokenType.TAG,)}
+
+        def _parse_tag(self, tag: str) -> Tag:
+            """ Parse HTML tags as if they are normal attributes/columns """
+            def _parse_tagattr(
+                name_or_namevalue: str,
+                value: t.Optional[str] = None
+            ) -> TagAttr:
+                """ Convert 'name="value"' string into TagAttr expression """
+                if not value:
+                    name_or_namevalue, value = name_or_namevalue.split('=', 1)
+                return self.expression(
+                    TagAttr,
+                    this=name_or_namevalue,
+                    expression=value.lstrip("r").strip("\""),
+                    is_regular=value.startswith("r\"")
+                )
+
+            #  INFO: bs4 can't properly handle r-prefixed attrs with w/spaces
+            name, attrs, *_ = tag.strip('<>').split(' ', 1) + ['']
+            expressions = [_parse_tagattr(x) for x in re.findall(
+                r'[a-z]+=r?".*?(?=" )"', attrs + " "
+            )]
+            return self.expression(Tag, this=HTMLTag(name), expressions=expressions)
 
         def _parse_loadpage(self, e: exp.Expression) -> exp.Expression:
             """ Parse LOAD expression
@@ -85,6 +261,13 @@ class ScrabyDialect(Dialect):
                 e = self._parse_loadpage(e)
             return e
 
+        def _parse_identifier(self) -> t.Optional[exp.Expression]:
+            if Tag.__name__ in self._curr.comments and self._match(TokenType.VAR):
+                #  INFO: dont forget to remove this TAG comment
+                self._prev.comments.remove(Tag.__name__)
+                return self._parse_tag(self._prev.text)
+            return super()._parse_identifier()
+
 
     #  INFO: overriden in terms of defining custom parser & generator properties
     def __init__(self, *args, **kwargs) -> None:
@@ -103,7 +286,7 @@ class ScrabyDialect(Dialect):
         
         """
         tokens = self.tokenize(sql)
-        print('\n'.join(str(x) for x in tokens))
+        # print('\n'.join(str(x) for x in tokens))
 
         #  FIX: sqlglot & SQL dont handle tags, so it should be parsed manually
         #  WARNING: end counts as included: ... text: <, start: 13, end: 13, ...
@@ -114,8 +297,7 @@ class ScrabyDialect(Dialect):
             #  if starts with "<", close to next and contained in TAGS
             if x.token_type == TokenType.LT \
             and tokens[i+1].start == x.end + 1 \
-            and tokens[i+1].text in self.TAGS:
-                print(x.text)
+            and tokens[i+1].text in HTMLTag.values():
                 tag_tokens: list[Token] = []
                 _tokens = deepcopy(tokens[i:])
                 while _tokens:
@@ -138,12 +320,13 @@ class ScrabyDialect(Dialect):
                         text=sql[tag_tokens[0].start:tag_tokens[-1].end+1],
                         line=tag_tokens[0].line,
                         start=tag_tokens[0].start,
-                        end=tag_tokens[-1].end+1
+                        end=tag_tokens[-1].end+1,
+                        comments=['Tag']
                     )]
 
-        print('\n' + '\n'.join(str(x) for x in tokens))
         result = self.parser(**opts).parse(tokens, sql)
         return result
+
 
 dialect = ScrabyDialect()
 
